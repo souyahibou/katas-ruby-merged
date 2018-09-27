68faf5e9583424c7fdb8bb67e61ff3d2d70f01d4
Souyahibou <souaybou.k@live.fr>
2018-09-27 15:53:58 +0200

    finish all questions ans implementations

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,13 +1,11 @@
-
-
-
 sequence = [4, 3, 5, 0, 1]
 swaps = 0
 
-# Your Code Here
+#Your Code Here
+
+#1 Write an implementation of bubble sort that does not use any custom classes. You'll likely want to use methods and will surely needs arrays and a while loop.
 result = []
-def bubble_sort( tab=[])
-    tail = tab.length
+def bubble_sort( tab)
     i=1
     cpt=1		
     swaps_nbr=0
@@ -22,19 +20,48 @@ def bubble_sort( tab=[])
         i+=1
         cpt+=1
     end
-#   puts swaps_nbr.to_s + " swaps"
-#   puts cpt.to_s + " iterations"
-#   puts tab.to_s + " array sorted"
-   return tab, swaps_nbr, cpt
+    return tab, swaps_nbr, cpt
 end
 
-result, swaps, cpt = bubble_sort(sequence)
+
+#2 what would be the worst case scenario for bubble sort? #=> the worst case scenario for bubble sort is [5, 4, 3, 1, 0]
+
+
+#3 How many swaps would that worst case take?  #=>the worst case take 10 swaps and 25 iterations(while)
+
+
+#4 Can you tweak the algorithm so that it takes the same number of swaps (7) but fewer total operations?   #=> 
+def bubble_sort_2( tab=[])
+    j=0
+    cpt=1		
+    swaps_nbr=0
+    while tab[j]                    #tant que l'on est pas à la fin du tableau à partir de l'indice 0
+	i=j+1                       #on commence le tri à partir d'un indice j+1 (et non plus au début(indice 1) comme précédemment)
+        while tab[i]                #tant que l'on a pas atteint la fin du tableau à partir du dernier indice non trié
+            if tab[i] < tab[j]      #tri si case index j est plus grand que le suivant 
+	        temp = tab[i]
+                tab[i] = tab[j]
+                tab[j] = temp
+                swaps_nbr+=1
+            end
+            i+=1
+            cpt+=1
+        end
+        j+=1
+    end
+    return tab, swaps_nbr, cpt
+end
+
 #end code
 
 
-
+result, swaps, cpt = bubble_sort sequence
 puts "Final result: #{result}"
 puts "Swaps: #{swaps}"
 puts "iterations : #{cpt}"
 
-# the worst case scenario for bubble sort is [5, 4, 3, 1, 0]
+result, swaps, cpt = bubble_sort_2 sequence = [4, 3, 5, 0, 1]
+puts "Final result: #{result}"
+puts "Swaps: #{swaps}"
+puts "iterations : #{cpt}"
+
