4a4bc6e6b89fd18cb33185da17a43e22a4e814de
Souyahibou <souaybou.k@live.fr>
2018-09-27 14:18:37 +0200

    finish first implementation

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,8 +1,14 @@
 def bubble_sort( tab=[] )
     tail = tab.length
+    i=1
     while ( tab[i] )
-        if tab[i-1] < tab[i]
-            tab[i]
+        if tab[i-1] > tab[i]
+	    temp = tab[i]
+            tab[i] = tab[i-1]
+            tab[i-1] = temp
+            i=0
         end
+        i+=1
     end
+    tab
 end
